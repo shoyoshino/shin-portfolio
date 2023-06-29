32df643e5dc146aa1be655ad333b0b66c4652e87
Sho Yoshino <shoyoshino@icloud.com>
2023-06-29 15:40:32 +0900

    first commit

---
+++ src/components/BlogPage.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const BlogPage = () => {
+  return (
+    <div className="container">
+      <h1>ブログ</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum veniam quis quo facere commodi numquam nostrum
+        labore, ullam optio consequuntur expedita eum sequi. Placeat dolore id ipsum inventore porro dicta delectus!
+        Delectus, ipsa voluptates eum odit suscipit pariatur officiis. Porro corporis deserunt tenetur vero maxime,
+        sequi excepturi, nam assumenda, vel in iusto aut et! Ipsa assumenda quia vel saepe laudantium quam sit amet
+        consequuntur, quaerat alias sequi labore obcaecati nemo maiores. Voluptas quo ad rem ut autem fuga dolore,
+        labore obcaecati magni eligendi dolor reiciendis tempore nulla necessitatibus adipisci vel, impedit, porro
+        maiores enim inventore? Quidem expedita tempore voluptate repudiandae.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum veniam quis quo facere commodi numquam nostrum
+        labore, ullam optio consequuntur expedita eum sequi. Placeat dolore id ipsum inventore porro dicta delectus!
+        Delectus, ipsa voluptates eum odit suscipit pariatur officiis. Porro corporis deserunt tenetur vero maxime,
+        sequi excepturi, nam assumenda, vel in iusto aut et! Ipsa assumenda quia vel saepe laudantium quam sit amet
+        consequuntur, quaerat alias sequi labore obcaecati nemo maiores. Voluptas quo ad rem ut autem fuga dolore,
+        labore obcaecati magni eligendi dolor reiciendis tempore nulla necessitatibus adipisci vel, impedit, porro
+        maiores enim inventore? Quidem expedita tempore voluptate repudiandae.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum veniam quis quo facere commodi numquam nostrum
+        labore, ullam optio consequuntur expedita eum sequi. Placeat dolore id ipsum inventore porro dicta delectus!
+        Delectus, ipsa voluptates eum odit suscipit pariatur officiis. Porro corporis deserunt tenetur vero maxime,
+        sequi excepturi, nam assumenda, vel in iusto aut et! Ipsa assumenda quia vel saepe laudantium quam sit amet
+        consequuntur, quaerat alias sequi labore obcaecati nemo maiores. Voluptas quo ad rem ut autem fuga dolore,
+        labore obcaecati magni eligendi dolor reiciendis tempore nulla necessitatibus adipisci vel, impedit, porro
+        maiores enim inventore? Quidem expedita tempore voluptate repudiandae.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum veniam quis quo facere commodi numquam nostrum
+        labore, ullam optio consequuntur expedita eum sequi. Placeat dolore id ipsum inventore porro dicta delectus!
+        Delectus, ipsa voluptates eum odit suscipit pariatur officiis. Porro corporis deserunt tenetur vero maxime,
+        sequi excepturi, nam assumenda, vel in iusto aut et! Ipsa assumenda quia vel saepe laudantium quam sit amet
+        consequuntur, quaerat alias sequi labore obcaecati nemo maiores. Voluptas quo ad rem ut autem fuga dolore,
+        labore obcaecati magni eligendi dolor reiciendis tempore nulla necessitatibus adipisci vel, impedit, porro
+        maiores enim inventore? Quidem expedita tempore voluptate repudiandae.
+      </p>
+    </div>
+  );
+};
+
+export default BlogPage;
